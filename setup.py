b4edbb74843ea569b24ee29f42a70bc2e41f9e4a
Itamar Turner-Trauring <itamar@futurefoundries.com>
2013-03-22 13:26:53 -0400

    First pass setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='crochet',
+    version='0.1',
+    description="Asynchronous calls for threaded applications",
+    install_requires=[
+        "Twisted>=11.0",
+        ],
+    keywords="twisted threading",
+    license="MIT",
+    packages=["crochet"],
+    url="https://github.com/itamarst/crochet",
+    maintainer='Itamar Turner-Trauring',
+    maintainer_email='itamar@futurefoundries.com',
+    long_description=open('README.md').read(),
+)
