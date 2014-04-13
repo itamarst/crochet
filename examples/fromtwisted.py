7a3a5ec0e15150063a53f19940f35f744c4f7a69
Itamar Turner-Trauring <itamar@itamarst.org>
2014-04-13 17:44:17 -0400

    A less contrieved example of using Crochet from a normal Twisted program.

--- examples/fromtwisted.py
+++ examples/fromtwisted.py
@@ -3,43 +3,41 @@
 An example of using Crochet from a normal Twisted application.
 """
 
-import time
-import threading
 import sys
 
-from crochet import no_setup, run_in_reactor, TimeoutError
+from crochet import no_setup, wait_for
 # Tell Crochet not to run the reactor:
 no_setup()
 
 from twisted.internet import reactor
-from twisted.web.client import getPage
+from twisted.python import log
+from twisted.web.wsgi import WSGIResource
+from twisted.web.server import Site
+from twisted.names import client
 
+# A WSGI application, will be run in thread pool:
+def application(environ, start_response):
+    start_response('200 OK', [])
+    try:
+        ip = gethostbyname('twistedmatrix.com')
+        return "%s has IP %s" % ('twistedmatrix.com', ip)
+    except Exception, e:
+        return 'Error doing lookup: %s' % (e,)
 
-# Blocking API:
-@run_in_reactor
-def _download_page(url):
-    return getPage(url)
+# A blocking API that will be called from the WSGI application, but actually
+# uses DNS:
+@wait_for(timeout=10)
+def gethostbyname(name):
+    d = client.lookupAddress(name)
+    d.addCallback(lambda result: result[0][0].payload.dottedQuad())
+    return d
 
-def time_page_download(url):
-    start = time.time()
-    _download_page(url).wait(1)
-    return time.time() - start
-
-
-def main(url):
-    def blockingCode():
-        print "Downloading", url
-        while True:
-            try:
-                elapsed = time_page_download(url)
-            except TimeoutError:
-                print "Timeout!"
-            else:
-                print "Download took", elapsed, "seconds"
-    threading.Thread(target=blockingCode).start()
-    # Run the reactor as you usually would:
+# Normal Twisted code, serving the WSGI application and running the reactor:
+def main():
+    log.startLogging(sys.stdout)
+    pool = reactor.getThreadPool()
+    reactor.listenTCP(5000, Site(WSGIResource(reactor, pool, application)))
     reactor.run()
 
-
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
