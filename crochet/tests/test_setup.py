1a469637e390bde0341900d13e500a7257cac22e
Itamar Turner-Trauring <itamar@itamarst.org>
2016-04-13 15:18:12 -0400

    Tests for logging integration with twisted.logger.

--- crochet/tests/test_setup.py
+++ crochet/tests/test_setup.py
@@ -338,3 +338,72 @@ if "twisted.internet.reactor" not in sys.modules:
         process = subprocess.Popen([sys.executable, "-c", program],
                                    cwd=crochet_directory)
         self.assertEqual(process.wait(), 23)
+
+
+LOGGING_PROGRAM = """\
+import sys
+from logging import StreamHandler, Formatter, getLogger, DEBUG
+handler = StreamHandler(sys.stdout)
+handler.setFormatter(Formatter("%%(levelname)s %%(message)s"))
+l = getLogger("twisted")
+l.addHandler(handler)
+l.setLevel(DEBUG)
+
+import crochet
+crochet.setup()
+from twisted.python import log
+%s
+log.msg("log-info")
+log.msg("log-error", isError=True)
+"""
+
+class LoggingTests(TestCase):
+    """
+    End-to-end tests for Twisted->stdlib logging bridge.
+    """
+    maxDiff = None
+
+    def test_old_logging(self):
+        """
+        Messages from the old Twisted logging API are emitted to Python
+        standard library logging.
+        """
+        if tuple(map(int, twisted.__version__.split("."))) >= (15, 2, 0):
+            raise SkipTest("This test is for Twisted < 15.2.")
+
+        program = LOGGING_PROGRAM % ("",)
+        output = subprocess.check_output([sys.executable, "-u", "-c", program],
+                                         cwd=crochet_directory)
+        self.assertTrue(output.startswith("""\
+INFO Log opened.
+INFO log-info
+ERROR log-error
+"""))
+
+    def test_new_logging(self):
+        """
+        Messages from both new and old Twisted logging APIs are emitted to Python
+        standard library logging.
+        """
+        if tuple(map(int, twisted.__version__.split("."))) < (15, 2, 0):
+            raise SkipTest("This test is for Twisted 15.2 and later.")
+
+        program = LOGGING_PROGRAM % ("""\
+from twisted.logger import Logger
+l2 = Logger()
+l2.info("logger-info")
+l2.critical("logger-critical")
+l2.warn("logger-warning")
+l2.debug("logger-debug")
+""",)
+        output = subprocess.check_output([sys.executable, "-u", "-c", program],
+                                         cwd=crochet_directory)
+        self.assertIn("""\
+INFO logger-info
+CRITICAL logger-critical
+WARNING logger-warning
+DEBUG logger-debug
+INFO log-info
+CRITICAL log-error
+""", output.decode("utf-8"))
+
